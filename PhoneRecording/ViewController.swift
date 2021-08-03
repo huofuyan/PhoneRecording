19a72461c07a8a77eb0288be1263b4a701aa5d3a
huofuyan <huohuo_0000@163.com>
2021-08-03 15:40:13 +0800

    推送目标项目

---
+++ PhoneRecording/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PhoneRecording
+//
+//  Created by cihonwuxian on 2021/8/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        
+    }
+    func precondition(index: Int, str: String){
+        
+    }
+    
+
+}
+
